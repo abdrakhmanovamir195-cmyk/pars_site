1f9208cfb837d37da6ddea0ff0922073211416b4
Amir <abdrakhmanovamir195@gmail.com>
2026-05-20 22:23:50 +0300

    add network

--- scraper/celery.py
+++ scraper/celery.py
@@ -1,7 +1,17 @@
 import os
+
+
+class Celery:
+    pass
+
+
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scraper.settings')
 app = Celery('scraper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+def shared_task():
+    return None
